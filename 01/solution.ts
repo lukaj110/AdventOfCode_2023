9f887ab4edbe73c0c342b7d44890acc357882260
lukaj110 <jovanovic.l01@htlwienwest.at>
2023-12-03 18:07:42 +0100

    annoying and overengineered day 1

---
+++ 01/solution.ts
@@ -0,0 +1,108 @@
+// Input
+
+const text = await Deno.readTextFile("./input.txt");
+
+const input = text.split("\n");
+
+// Part 1
+
+let sum = 0;
+
+for (const line of input) {
+  const numbers = line.split("").filter((e) => !isNaN(parseInt(e)));
+
+  if (numbers.length === 0) continue;
+
+  const [firstNumber, lastNumber] = [numbers[0], numbers[numbers.length - 1]];
+
+  const fullNumber = parseInt(`${firstNumber}${lastNumber}`);
+
+  sum += fullNumber;
+}
+
+console.log(sum);
+
+// Part 2
+
+let sum2 = 0;
+
+const numbers = Array.from({ length: 9 }, (_, e) => e + 1);
+
+const numberDictionary: Record<string, number> = {
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+for (const line of input) {
+  const numbersIndices = numbers
+    .map((e) => line.indexOf(e.toString()))
+    .map((e) => {
+      if (e === -1) return 999;
+      return e;
+    });
+
+  const lowestNumberIndex = Math.min(...numbersIndices);
+
+  const textNumbersIndices = Object.keys(numberDictionary)
+    .map((e) => line.indexOf(e))
+    .map((e) => {
+      if (e === -1) return 999;
+      return e;
+    });
+
+  const lowestTextNumberIndex = Math.min(...textNumbersIndices);
+
+  let lowestNumber = 0;
+
+  if (lowestNumberIndex < lowestTextNumberIndex) {
+    lowestNumber =
+      lowestNumberIndex !== 999
+        ? numbers[numbersIndices.indexOf(lowestNumberIndex)]
+        : Object.values(numberDictionary)[
+            textNumbersIndices.indexOf(lowestTextNumberIndex)
+          ];
+  } else {
+    lowestNumber =
+      lowestTextNumberIndex !== 999
+        ? Object.values(numberDictionary)[
+            textNumbersIndices.indexOf(lowestTextNumberIndex)
+          ]
+        : numbers[numbersIndices.indexOf(lowestNumberIndex)];
+  }
+
+  const numbersIndicesHighest = numbers.map((e) =>
+    line.lastIndexOf(e.toString())
+  );
+
+  const highestNumberIndex = Math.max(...numbersIndicesHighest);
+
+  const textNumbersIndicesHighest = Object.keys(numberDictionary).map((e) =>
+    line.lastIndexOf(e)
+  );
+
+  const highestTextNumberIndex = Math.max(...textNumbersIndicesHighest);
+
+  let highestNumber = 0;
+
+  if (highestNumberIndex > highestTextNumberIndex) {
+    highestNumber = numbers[numbersIndicesHighest.indexOf(highestNumberIndex)];
+  } else {
+    highestNumber =
+      Object.values(numberDictionary)[
+        textNumbersIndicesHighest.indexOf(highestTextNumberIndex)
+      ];
+  }
+
+  const finalNumber = parseInt(`${lowestNumber}${highestNumber}`);
+
+  sum2 += finalNumber;
+}
+
+console.log(sum2);
---
+++ README.md
@@ -0,0 +1,11 @@
+# AdventOfCode - 2023
+
+This repository contains my solutions to the [Advent of Code 2023](https://adventofcode.com/2023) challenges.
+
+## Usage
+
+To run the solutions, you need to have [Deno](https://deno.land/manual@v1.28.3/getting_started/installation) installed. Then, you can run the solutions with the following command:
+
+```bash
+deno run --allow-read solution.ts
+```
